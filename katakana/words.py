8f9699d4e5a9ee552ddf73c87da97e0f26ec4179
NathanPERIER <periernathan2000@gmail.com>
2023-04-18 23:31:05 +0200

    Added some words translations for later

---
+++ katakana/words.py
@@ -0,0 +1,48 @@
+
+{
+    'キャンセル': 'cancel',
+    'キス': 'kiss',
+    'キリン': 'giraffe',
+    'キャンバス': 'canvas',
+    'キャンセル': 'cancel',
+    'キャンプ': 'camp',
+    'キャラクター': 'character',
+    'チョコレート': 'chocolate',
+    'チョーク': 'chalk',
+    'ボス': 'boss',
+    'バレンタインデー': 'valentain\'s day',
+    'クラス': 'class',
+    'クラブ': 'club',
+    'クラシック': 'classic',
+    'クラゲ': 'jellyfish',
+    'クレジット': 'credit',
+    'クレイジー': 'crazy',
+    'クレープ': 'crêpe',
+    'ゲーム': 'game',
+    'クレーンゲーム': 'crane game',
+    'ゲームセンター': 'game center',
+    'アーケード': 'arcade',
+    'ケーキ': 'cake',
+    'ケチャップ': 'ketchup',
+    'ソース': 'sauce',
+    'ソーセージ': 'sausage',
+    'ソーダ': 'soda',
+    'ソーシャル': 'social',
+    'ソフト': 'soft',
+    'ソフトウェア': 'software',
+    'ソファー': 'sofa',
+    'コーヒー': 'coffee',
+    'コンビニ': 'konbini',
+    'ストア': 'store',
+    'コンサート': 'concert',
+    'コントロール': 'control',
+    'コンピューター': 'computer'
+
+}
+
+"""
+sofutowa-ru
+konpyu-ta-
+ha-do doraibu
+"""
+
